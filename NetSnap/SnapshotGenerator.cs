cb66c11c74fe790ed3bc4ae0131f9d31ff2dc696
DimaInNature <78268612+DimaInNature@users.noreply.github.com>
2026-01-13 22:57:15 +0400

    Fix extensions

--- NetSnap/SnapshotGenerator.cs
+++ NetSnap/SnapshotGenerator.cs
@@ -6,7 +6,7 @@ public static class SnapshotGenerator
 
     private static readonly string[] DefaultIgnoredFiles = [".gitattributes", ".gitignore", "snapshot.txt"];
 
-    private static readonly string[] DefaultIgnoredExtensions = [".img", ".jpeg", ".webp"];
+    private static readonly string[] DefaultIgnoredExtensions = [".img", ".jpeg", ".webp", ".dll", ".zip", ".pdf", ".ico", ".bmp", ".gif", ".jpg", ".exe", ".pdb", ".so", ".dylib", ".wasm", ".ttf", ".otf", ".woff", ".woff2"];
 
     public static string CreateSnapshot(string sourcePath, string outputFile)
     {
